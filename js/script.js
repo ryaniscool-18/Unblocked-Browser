c185bf079a64fc5045ed0f43613f2a81d7470933
ryaniscool-18 <ryan.antony_ry@outlook.com>
2024-09-27 13:30:54 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
-// scripts.js
-console.log("Welcome to Online Unblocked browser!");
-
+document.getElementById('go-button').addEventListener('click', function() {
+    const url = document.getElementById('url-input').value;
+    document.getElementById('web-frame').src = url.startsWith('http') ? url : 'http://' + url;
+});
