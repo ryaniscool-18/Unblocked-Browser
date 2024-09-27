fd515b7118bbc9c6b96a4af106b3a53a5c13c197
ryaniscool-18 <ryan.antony_ry@outlook.com>
2024-09-27 14:43:37 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,15 @@
+const frame = document.getElementById('web-frame');
+const urlInput = document.getElementById('url-input');
+
 document.getElementById('go-button').addEventListener('click', function() {
-    const url = document.getElementById('url-input').value;
-    document.getElementById('web-frame').src = url.startsWith('http') ? url : 'http://' + url;
+    const url = urlInput.value;
+    frame.src = url.startsWith('http') ? url : 'http://' + url;
+});
+
+document.getElementById('back-button').addEventListener('click', function() {
+    frame.contentWindow.history.back();
+});
+
+document.getElementById('forward-button').addEventListener('click', function() {
+    frame.contentWindow.history.forward();
 });
